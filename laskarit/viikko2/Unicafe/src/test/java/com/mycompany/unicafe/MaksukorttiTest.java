0be48a62211e8cd5052041acd2a1845b64260b25
Fanni Franssila <fanni.franssila@gmail.com>
2019-03-19 16:39:32 +0200

    Lisättiin gitignore ja muokattiin Unicafe-projektin testejä.

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,40 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void lataaRahaaKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaVahentaaSaldoaOikeanMaaran() {
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 0.0", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaEiVahennaSaldoaJosRahaEiRiita() {
+        kortti.otaRahaa(20);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrueJosRahaRiitti() {
+        boolean tulos = kortti.otaRahaa(10);
+        assertTrue(tulos);
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalseJosRahaEiRiita() {
+        boolean tulos = kortti.otaRahaa(20);
+        assertTrue(!tulos);
+    }
+    
 }
